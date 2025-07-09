3a6ad2b53420ece662cbe0bc0d9a1f1509ab1e27
Thangleee <139549701+Thangleee@users.noreply.github.com>
2025-07-09 19:32:40 +0700

    Update Branch_Comp.v

--- Branch_Comp.v
+++ Branch_Comp.v
@@ -1,30 +1,22 @@
 module Branch_Comp (
-    input  logic [31:0] A,         
-    input  logic [31:0] B,         
-    input  logic        Branch,    
-    input  logic [2:0]  funct3,    
-    output logic        BrTaken    
+    input  logic [31:0] dataA,      // toán hạng 1
+    input  logic [31:0] dataB,      // toán hạng 2
+    input  logic        Branch,     // tín hiệu nhảy
+    input  logic [2:0]  funct3,     // loại lệnh nhảy
+    output logic        BrTaken     // cờ quyết định nhảy
 );
-
-    logic take_branch;
-
     always_comb begin
-        
-        take_branch = 1'b0;
-
+        BrTaken = 0;
         if (Branch) begin
-            unique case (funct3)
-                3'b000: take_branch = (A == B);                          // beq
-                3'b001: take_branch = (A != B);                          // bne
-                3'b100: take_branch = ($signed(A) < $signed(B));         // blt
-                3'b101: take_branch = ($signed(A) >= $signed(B));        // bge
-                3'b110: take_branch = (A < B);                           // bltu
-                3'b111: take_branch = (A >= B);                          // bgeu
-                default: take_branch = 1'b0;
+            case (funct3)
+                3'b000: BrTaken = (dataA == dataB);                    // beq
+                3'b001: BrTaken = (dataA != dataB);                    // bne
+                3'b100: BrTaken = ($signed(dataA) < $signed(dataB));  // blt
+                3'b101: BrTaken = ($signed(dataA) >= $signed(dataB)); // bge
+                3'b110: BrTaken = (dataA < dataB);                     // bltu
+                3'b111: BrTaken = (dataA >= dataB);                    // bgeu
+                default: BrTaken = 0;
             endcase
         end
-
-        BrTaken = take_branch;
     end
-
 endmodule
