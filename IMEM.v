d46b76020bfe02fbf7e8e60798efe42d16f1b3fb
Thangleee <139549701+Thangleee@users.noreply.github.com>
2025-07-09 18:36:53 +0700

    Update IMEM.v

--- IMEM.v
+++ IMEM.v
@@ -1,17 +1,12 @@
 module IMEM (
-    input  logic [31:0] addr,
-    output logic [31:0] Instruction
+    input  [31:0] addr,
+    output [31:0] Instruction
 );
-    logic [31:0] instr_mem [0:255];
+    reg [31:0] memory [0:255];
 
-    // Giới hạn SC1: nếu vượt quá 128 lệnh, trả về HALT (beq x0, x0, 0)
-    assign Instruction = (addr[11:2] < 7'd128) ? instr_mem[addr[11:2]] : 32'h00000063;
+    assign Instruction = (addr[11:2] < 128) ? memory[addr[11:2]] : 32'h00000063;
 
-    // Load từ file .hex
     initial begin
-        if (!$readmemh("./mem/imem2.hex", instr_mem)) begin
-            $display("imem2.hex not found. Falling back to imem.hex...");
-            $readmemh("./mem/imem.hex", instr_mem);
-        end
+        $readmemh("./mem/imem2.hex", memory);
     end
 endmodule
