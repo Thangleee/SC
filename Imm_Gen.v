c72d14c5e20da75b263abccfb2e07d61bf9aeef9
Thangleee <139549701+Thangleee@users.noreply.github.com>
2025-07-09 18:43:55 +0700

    Update Imm_Gen.v

--- Imm_Gen.v
+++ Imm_Gen.v
@@ -1,34 +1,32 @@
 module Imm_Gen (
-    input  logic [31:0] inst,
-    output logic [31:0] imm_out
+    input [31:0] inst,
+    output reg [31:0] imm_out
 );
-    logic [6:0] opcode;
+    wire [6:0] opcode;
     assign opcode = inst[6:0];
 
-    always_comb begin
+    always @(*) begin
         case (opcode)
-            // I-type: lw, addi, jalr
-            7'b0000011, 7'b0010011, 7'b1100111:
+            7'b0000011, // I-type (load)
+            7'b0010011,
+            7'b1100111:
                 imm_out = {{20{inst[31]}}, inst[31:20]};
 
-            // S-type: sw
-            7'b0100011:
+            7'b0100011: // S-type
                 imm_out = {{20{inst[31]}}, inst[31:25], inst[11:7]};
 
-            // B-type: beq, bne, blt, bge...
-            7'b1100011:
+            7'b1100011: // B-type
                 imm_out = {{19{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0};
 
-            // U-type: lui, auipc
-            7'b0010111, 7'b0110111:
-                imm_out = {inst[31:12], 12'd0};
+            7'b0010111, // auipc
+            7'b0110111: // lui
+                imm_out = {inst[31:12], 12'b0};
 
-            // J-type: jal
-            7'b1101111:
+            7'b1101111: // jal
                 imm_out = {{11{inst[31]}}, inst[31], inst[19:12], inst[20], inst[30:21], 1'b0};
 
             default:
-                imm_out = 32'd0;
+                imm_out = 32'b0;
         endcase
     end
 endmodule
